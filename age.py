f121e8ceebe95d82541006c0de7233f7734a53f7
derryev <derryev@ssfs.org>
2023-09-25 19:52:32 -0400

    Wrote program that tells user when they will turn 100 based on their age and the current year.

---
+++ age.py
@@ -0,0 +1,5 @@
+name = input("What is your name?")
+age = input("How old are you?")
+year = input("What year is it?")
+century_birthday = float(year)-float(age)+100
+print(name,"will turn 100 in",century_birthday)
