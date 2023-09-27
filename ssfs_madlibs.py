061c9b55e395c9f9980246f1026776f8f1943bf4
derryev <derryev@ssfs.org>
2023-09-27 08:42:29 -0400

    Adlib.

---
+++ ssfs_madlibs.py
@@ -0,0 +1,19 @@
+
+verb1 = input("Verb ending in -ing:")
+name1 = input("Name of someone at SSFS:")
+adverb1 = input("Adverb:")
+name2 = input("Name of a different person at SSFS:")
+verb2 = input("Verb ending in -ing:")
+adjective1 = input("Adjective:")
+adverb2 = input("Adverb:")
+verb3 = input("Verb ending in -ing:")
+verb4 = input("Verb ending in -ed:")
+adverb3 = input("Adverb ending in -ly:")
+adverb4 = input("Adverb:")
+
+
+print("I was",verb1,"to Yarnall when I looked over my shoulder and saw a wildebeest attacking",name1,". I ran",adverb1,
+      "to the scene, trying to understand what was going on. When I reached the sidewalk, I realized that it wasn't a"
+      "wildebeest, but instead",name1,"and",name2,"fighting. Apparently",name2,"was",verb2,"in meeting for worship,"
+      "which made",name1,"feel",adjective1,". I reminded them to think of what George Fox would do, and they",adverb2,
+      "stopped fighting and started",verb3,". I",verb4,"away",adverb3,", Walk in the Light playing",adverb4,"behind me.")
