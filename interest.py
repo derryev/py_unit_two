bfd05c926dcdc55b06d8c18f705a2d0e72547595
derryev <derryev@ssfs.org>
2023-09-27 08:57:20 -0400

    Completed compound interest calculator.

--- interest.py
+++ interest.py
@@ -1,2 +1,10 @@
-# Use this file to calculate the amount of compound interest. Follow the instructions on the
-# Variables and Expressions sheet carefully.
+#Eva D
+#september 27 2023
+# Compund interest calculator
+principal_amount_p = 10000
+n = 12
+interest_rate_r = .08
+years_t = 10
+
+final_amount_a = principal_amount_p*(1+(interest_rate_r/n))**(n*years_t)
+print("The amount of compound interest is",final_amount_a,"dollars.")
