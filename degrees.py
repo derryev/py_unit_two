3f7083f0b921195e82a6719b71a909f26c9c21a2
derryev <derryev@ssfs.org>
2023-09-27 08:51:21 -0400

    Made C to F calculator.

---
+++ degrees.py
@@ -0,0 +1,6 @@
+#Eva D
+#september 27 2023
+# Celsius to Farenheit calculator
+temp_c = input("Enter a temperature in Celsius:")
+temp_f = 9/5*float(temp_c)+32
+print(temp_c,"degrees Celsius is",temp_f,"degrees Fahrenheit.")
